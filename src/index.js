989b44605756ca4cc086a7d4fd5a364b4fb54539
Faizan <faizanamjad575@gmail.com>
2024-06-07 23:34:31 +0500

    Make menu button functional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './style.css';
 import buildHomeTab from './home';
+import buildMenuTab from './menu';
 buildHomeTab();
+
+const menuButton = document.getElementById('menu');
+
+menuButton.onclick = function(){
+    //remove current tab's content
+    const myNode = document.getElementById('content');
+    while (myNode.lastElementChild) {
+        myNode.removeChild(myNode.lastElementChild);
+    }
+
+    buildMenuTab();//add menu tab's content
+}
