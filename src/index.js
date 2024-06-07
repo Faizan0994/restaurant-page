72a636fdf864cf42dfc2afaf12f7d52d99a5ec73
Faizan <faizanamjad575@gmail.com>
2024-06-07 23:37:19 +0500

    add functionality to go back to home tab

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import buildHomeTab from './home';
 import buildMenuTab from './menu';
 buildHomeTab();
 
+const homeButton = document.getElementById('home');
 const menuButton = document.getElementById('menu');
+
+homeButton.onclick = function(){
+    //remove current tab's content
+    const myNode = document.getElementById('content');
+    while (myNode.lastElementChild) {
+        myNode.removeChild(myNode.lastElementChild);
+    }
+
+    buildHomeTab();//add menu tab's content
+}
 
 menuButton.onclick = function(){
     //remove current tab's content
